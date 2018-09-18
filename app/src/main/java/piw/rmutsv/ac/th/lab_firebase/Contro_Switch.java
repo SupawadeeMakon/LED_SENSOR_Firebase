befa0d0cdfdc4aee7ffc6e855321bf8070d338c4
Supawadee <supawadee018@gmail.com>
2018-09-18 12:16:40 +0700

    ดึงค่าจากfirebase จาก sensor มาแสดง

--- app/src/main/java/piw/rmutsv/ac/th/lab_firebase/Contro_Switch.java
+++ app/src/main/java/piw/rmutsv/ac/th/lab_firebase/Contro_Switch.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -13,14 +14,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Map;
+
 public class Contro_Switch extends AppCompatActivity {
 
 
     public FirebaseDatabase firebaseDatabase;
-    public DatabaseReference databaseReference;
+    public DatabaseReference databaseReference,datagetFirebase;
     private static final String TAG = "Switch LED";
     private Button Switch1;
     public Integer Value, Value_refer;
+
+    private TextView textViewdata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +61,30 @@ public class Contro_Switch extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 databaseReference.setValue(Value_refer);
+            }
+        });
+
+
+        //Get data from Sensor
+
+        //ประกาศ id ของ textView
+        textViewdata = (TextView) findViewById(R.id.txtFirebase);
+
+        //สร้างตัวรับข้อมูล Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        datagetFirebase = database.getReference();
+        datagetFirebase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {//เรียกข้อมูลที่เกิดการเปลี่ยนแปลงทุกครั้งของค่าข้อมูลใน Path ที่อ้างถึง
+                Map map = (Map)dataSnapshot.getValue() ; //ดึงค่าจาก Firebase
+                String data1 =String.valueOf(map.get("Senser_Y401")); //ดึงค่าที่อยู่ใน Path
+                textViewdata.setText(data1);//แสดงค่า
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {//ไม่สามารถอ่านค่าข้อมูลในdatabase ได้
+
             }
         });
 
